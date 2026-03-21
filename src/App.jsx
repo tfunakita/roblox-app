67906ef73bcef51b776137848bffb981ecee97fc
船木鷹也 <funakitakaya@UNIQLO.local>
2026-03-21 20:35:53 +0900

    fix: BrowserRouter→HashRouterに変更（GitHub Pages直アクセス404対策）

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { UnlockedProvider } from './hooks/useUnlocked'
 import PortalPage from './pages/PortalPage'
 
 export default function App() {
   return (
     <UnlockedProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="*" element={<PortalPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </UnlockedProvider>
   )
 }
